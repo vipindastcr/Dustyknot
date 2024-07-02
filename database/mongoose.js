8e35965d9aff39f3b6c7e8b4cdb25e06aeb909f7
vipindastcr <vpn9995@gmail.com>
2024-07-02 23:12:28 +0530

    mongoose connect change

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,8 @@
 
 const mongoose = require("mongoose");
 function connectDb (){
-    const mongo = mongoose.connect('mongodb://localhost:27017/dustyknotDB');
+    // const mongo = mongoose.connect('mongodb://localhost:27017/dustyknotDB');
+    const mongo = mongoose.connect('mongodb+srv://vipindasDustyknot:12345678AsD@cluster0.5tl42z0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     mongo.then(()=> {
         console.log("Database connected");
     })
