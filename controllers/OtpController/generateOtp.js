54f1397d5bb38f35d43ea7a0725e87ac48c5caa9
vipindastcr <vpn9995@gmail.com>
2024-07-17 15:25:59 +0530

    otp code cleaned

--- controllers/OtpController/generateOtp.js
+++ controllers/OtpController/generateOtp.js
@@ -3,13 +3,11 @@ const express = require('express');
 
 const generateOtp = function() {
     let Otp = "";
-    console.log("gettting otp");
 
     for(let i=0; i<6; i++) {
         Otp += Math.floor(Math.random()*10)
     }
     
-    console.log("evide ethiyi000..???"+Otp);
     return Otp;
 }
 
